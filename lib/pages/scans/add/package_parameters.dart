780a0fca0d106b519d1fba52d4b172364de93ce4
Darya Varabei <daria14072002@gmail.com>
2022-03-15 00:40:12 +0300

    add: navigation components

--- lib/pages/scans/add/package_parameters.dart
+++ lib/pages/scans/add/package_parameters.dart
@@ -18,6 +18,46 @@ class _PackageParametersPageState extends State<PackageParametersPage> {
       ),
       body: Center(
           child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Flexible(
+                flex: 1,
+                child: Container(
+                  width: MediaQuery.of(context).size.width - 80,
+                  height: double.infinity,
+                  alignment: Alignment.centerRight,
+                  child: Row(children: <Widget>[
+                    IconButton(
+                      icon: const Icon(Icons.arrow_back),
+                      onPressed: () {},
+                    ),
+                    Spacer(),
+                    SizedBox(
+                      width: 190,
+                      child: Text(
+                        "Сертификат № 257165765",
+                        textAlign: TextAlign.right,
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: AppTheme.colors.darkGradient,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    ),
+                  ]),
+                )),
+            Flexible(
+                flex: 1,
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
+                  alignment: Alignment.centerRight,
+                  child: Column(children: <Widget>[
+                    Text(
+                      "56578",
+                      style: TextStyle(
+                          fontSize: 24,
+                          color: AppTheme.colors.darkGradient,
+                          fontWeight: FontWeight.w400),
+                    ),
+                  ]),
+                )),
         Flexible(
           flex: 2,
           child: Container(
