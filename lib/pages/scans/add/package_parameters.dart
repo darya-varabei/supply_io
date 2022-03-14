f5ba5a580a6f99486e0358d47943f36bc04d9036
Darya Varabei <daria14072002@gmail.com>
2022-03-15 01:02:08 +0300

    add: buttons

--- lib/pages/scans/add/package_parameters.dart
+++ lib/pages/scans/add/package_parameters.dart
@@ -18,50 +18,50 @@ class _PackageParametersPageState extends State<PackageParametersPage> {
       ),
       body: Center(
           child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Flexible(
-                flex: 1,
-                child: Container(
-                  width: MediaQuery.of(context).size.width - 80,
-                  height: double.infinity,
-                  alignment: Alignment.centerRight,
-                  child: Row(children: <Widget>[
-                    IconButton(
-                      icon: const Icon(Icons.arrow_back),
-                      onPressed: () {},
-                    ),
-                    Spacer(),
-                    SizedBox(
-                      width: 190,
-                      child: Text(
-                        "Сертификат № 257165765",
-                        textAlign: TextAlign.right,
-                        style: TextStyle(
-                            fontSize: 16,
-                            color: AppTheme.colors.darkGradient,
-                            fontWeight: FontWeight.w400),
-                      ),
-                    ),
-                  ]),
-                )),
-            Flexible(
-                flex: 1,
-                child: Container(
-                  padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
-                  alignment: Alignment.centerRight,
-                  child: Column(children: <Widget>[
-                    Text(
-                      "56578",
-                      style: TextStyle(
-                          fontSize: 24,
-                          color: AppTheme.colors.darkGradient,
-                          fontWeight: FontWeight.w400),
-                    ),
-                  ]),
-                )),
         Flexible(
-          flex: 2,
+            flex: 1,
+            child: Container(
+              width: MediaQuery.of(context).size.width - 80,
+              height: double.infinity,
+              alignment: Alignment.centerRight,
+              child: Row(children: <Widget>[
+                IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () {},
+                ),
+                Spacer(),
+                SizedBox(
+                  width: 190,
+                  child: Text(
+                    "Сертификат № 257165765",
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                        fontSize: 16,
+                        color: AppTheme.colors.darkGradient,
+                        fontWeight: FontWeight.w400),
+                  ),
+                ),
+              ]),
+            )),
+        Flexible(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.fromLTRB(40, 8, 40, 10),
+              alignment: Alignment.centerRight,
+              child: Column(children: <Widget>[
+                Text(
+                  "56578",
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: AppTheme.colors.darkGradient,
+                      fontWeight: FontWeight.w400),
+                ),
+              ]),
+            )),
+        Flexible(
+          flex: 1,
           child: Container(
-            padding: EdgeInsets.fromLTRB(40, 20, 40, 10),
+            padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
             child: Row(children: <Widget>[
               Text(
                 "Марка стали",
@@ -83,155 +83,169 @@ class _PackageParametersPageState extends State<PackageParametersPage> {
             ]),
           ),
         ),
-            Flexible(
-              flex: 2,
-              child: Container(
-                padding: EdgeInsets.fromLTRB(40, 20, 40, 10),
-                child: Row(children: <Widget>[
-                  Text(
-                    "Производитель",
-                    textAlign: TextAlign.right,
-                    style: TextStyle(
-                        fontSize: 16,
-                        color: AppTheme.colors.darkGradient,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  Spacer(),
-                  Text(
-                    "Северсталь",
-                    textAlign: TextAlign.right,
-                    style: TextStyle(
-                        fontSize: 16,
-                        color: AppTheme.colors.darkGradient,
-                        fontWeight: FontWeight.w400),
-                  ),
-                ]),
+        Flexible(
+          flex: 1,
+          child: Container(
+            padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
+            child: Row(children: <Widget>[
+              Text(
+                "Производитель",
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: AppTheme.colors.darkGradient,
+                    fontWeight: FontWeight.w600),
               ),
-            ),
-            Flexible(
-              flex: 2,
-              child: Container(
-                padding: EdgeInsets.fromLTRB(40, 20, 40, 10),
-                child: Row(children: <Widget>[
-                  Text(
-                    "Толщина",
-                    textAlign: TextAlign.right,
-                    style: TextStyle(
-                        fontSize: 16,
-                        color: AppTheme.colors.darkGradient,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  Spacer(),
-                  Text(
-                    "1.2 мм",
-                    textAlign: TextAlign.right,
-                    style: TextStyle(
-                        fontSize: 16,
-                        color: AppTheme.colors.darkGradient,
-                        fontWeight: FontWeight.w400),
-                  ),
-                ]),
+              Spacer(),
+              Text(
+                "Северсталь",
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: AppTheme.colors.darkGradient,
+                    fontWeight: FontWeight.w400),
               ),
-            ),
-            Flexible(
-              flex: 1,
-              child: Container(
-                padding: EdgeInsets.fromLTRB(40, 20, 40, 10),
-                child: Row(children: <Widget>[
-                  Text(
-                    "Ширина",
-                    textAlign: TextAlign.right,
-                    style: TextStyle(
-                        fontSize: 16,
-                        color: AppTheme.colors.darkGradient,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  Spacer(),
-                  Text(
-                    "1230мм",
-                    textAlign: TextAlign.right,
-                    style: TextStyle(
-                        fontSize: 16,
-                        color: AppTheme.colors.darkGradient,
-                        fontWeight: FontWeight.w400),
-                  ),
-                ]),
+            ]),
+          ),
+        ),
+        Flexible(
+          flex: 1,
+          child: Container(
+            padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
+            child: Row(children: <Widget>[
+              Text(
+                "Толщина",
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: AppTheme.colors.darkGradient,
+                    fontWeight: FontWeight.w600),
               ),
-            ),
-            Flexible(
-              flex: 1,
-              child: Container(
-                padding: EdgeInsets.fromLTRB(40, 20, 40, 10),
-                child: Row(children: <Widget>[
-                  Text(
-                    "Масса(брутто)",
-                    textAlign: TextAlign.right,
-                    style: TextStyle(
-                        fontSize: 16,
-                        color: AppTheme.colors.darkGradient,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  Spacer(),
-                  Text(
-                    "4567 кг",
-                    textAlign: TextAlign.right,
-                    style: TextStyle(
-                        fontSize: 16,
-                        color: AppTheme.colors.darkGradient,
-                        fontWeight: FontWeight.w400),
-                  ),
-                ]),
+              Spacer(),
+              Text(
+                "1.2 мм",
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: AppTheme.colors.darkGradient,
+                    fontWeight: FontWeight.w400),
               ),
-            ),
-            Flexible(
-              flex: 1,
-              child: Container(
-                padding: EdgeInsets.fromLTRB(40, 20, 40, 10),
-                child: Row(children: <Widget>[
-                  Text(
-                    "Масса(нетто)",
-                    textAlign: TextAlign.right,
-                    style: TextStyle(
-                        fontSize: 16,
-                        color: AppTheme.colors.darkGradient,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  Spacer(),
-                  Text(
-                    "4466 кг",
-                    textAlign: TextAlign.right,
-                    style: TextStyle(
-                        fontSize: 16,
-                        color: AppTheme.colors.darkGradient,
-                        fontWeight: FontWeight.w400),
-                  ),
-                ]),
+            ]),
+          ),
+        ),
+        Flexible(
+          flex: 1,
+          child: Container(
+            padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
+            child: Row(children: <Widget>[
+              Text(
+                "Ширина",
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: AppTheme.colors.darkGradient,
+                    fontWeight: FontWeight.w600),
               ),
-            ),
-            // Flexible(
-            //   flex: 1,
-            //   child: Container(
-            //     padding: EdgeInsets.fromLTRB(40, 20, 40, 10),
-            //     child: Row(children: <Widget>[
-            //       Text(
-            //         "Марка стали",
-            //         textAlign: TextAlign.right,
-            //         style: TextStyle(
-            //             fontSize: 16,
-            //             color: AppTheme.colors.darkGradient,
-            //             fontWeight: FontWeight.w600),
-            //       ),
-            //       Spacer(),
-            //       Text(
-            //         "08Ю",
-            //         textAlign: TextAlign.right,
-            //         style: TextStyle(
-            //             fontSize: 16,
-            //             color: AppTheme.colors.darkGradient,
-            //             fontWeight: FontWeight.w400),
-            //       ),
-            //     ]),
-            //   ),
-            // ),
+              Spacer(),
+              Text(
+                "1230мм",
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: AppTheme.colors.darkGradient,
+                    fontWeight: FontWeight.w400),
+              ),
+            ]),
+          ),
+        ),
+        Flexible(
+          flex: 1,
+          child: Container(
+            padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
+            child: Row(children: <Widget>[
+              Text(
+                "Масса(брутто)",
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: AppTheme.colors.darkGradient,
+                    fontWeight: FontWeight.w600),
+              ),
+              Spacer(),
+              Text(
+                "4567 кг",
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: AppTheme.colors.darkGradient,
+                    fontWeight: FontWeight.w400),
+              ),
+            ]),
+          ),
+        ),
+        Flexible(
+          flex: 1,
+          child: Container(
+            padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
+            child: Row(children: <Widget>[
+              Text(
+                "Масса(нетто)",
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: AppTheme.colors.darkGradient,
+                    fontWeight: FontWeight.w600),
+              ),
+              Spacer(),
+              Text(
+                "4466 кг",
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: AppTheme.colors.darkGradient,
+                    fontWeight: FontWeight.w400),
+              ),
+            ]),
+          ),
+        ),
+        Flexible(
+            flex: 2,
+            child: Container(
+              padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+              child: Column(children: <Widget>[
+                FlatButton(
+                  padding: EdgeInsets.symmetric(vertical: 13, horizontal: 26),
+                  onPressed: () {},
+                  child: Text(
+                    "Внести изменения",
+                    style: TextStyle(color: AppTheme.colors.blue),
+                  ),
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                          color: AppTheme.colors.blue,
+                          width: 2,
+                          style: BorderStyle.solid),
+                      borderRadius: BorderRadius.circular(20)),
+                ),
+              ]),
+            )),
+        Flexible(
+            flex: 2,
+            child: Container(
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+              child: Column(children: <Widget>[
+                FlatButton(
+                  padding: EdgeInsets.symmetric(vertical: 13, horizontal: 54),
+                  onPressed: () {},
+                  child: Text(
+                    "Сохранить",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  color: AppTheme.colors.blue,
+                  shape: StadiumBorder(),
+                ),
+              ]),
+            )),
       ])));
 }
